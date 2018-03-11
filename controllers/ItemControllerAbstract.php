748453654e69eedadb3aa87ba327f1c554061004
Кирилл <snewer0@gmail.com>
2018-03-11 20:16:13 +0400

    fix output

--- controllers/ItemControllerAbstract.php
+++ controllers/ItemControllerAbstract.php
@@ -141,7 +141,7 @@ abstract class ItemControllerAbstract extends Controller
     {
         if (\Yii::$app->request->isAjax && $model->load(\Yii::$app->request->post())) {
             \Yii::$app->response->format = Response::FORMAT_JSON;
-            echo json_encode(ActiveForm::validate($model));
+            \Yii::$app->response->data = json_encode(ActiveForm::validate($model));
             \Yii::$app->end();
         }
     }
